6cfd8d79e2609722a4cc53cd1f4181f69ead2139
Younghwan <tacdo12@naver.com>
2022-06-05 03:29:22 +0900

    11. 유효성 검사 완료

--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -17,12 +17,12 @@ import com.cos.photogramstart.handler.ex.CustomValidationException;
 import com.cos.photogramstart.service.AuthService;
 import com.cos.photogramstart.web.dto.auth.SignupDto;
 
-@Controller	// 1. IoC	2. 파일을 리턴하는 컨트롤러 
+@Controller // 1. IoC 2. 파일을 리턴하는 컨트롤러
 public class AuthController {
-	
+
 	@Autowired
 	private AuthService authService;
-	
+
 	@GetMapping("/auth/signin")
 	public String signinForm() {
 		return "auth/signin";
@@ -32,21 +32,12 @@ public class AuthController {
 	public String signupForm() {
 		return "auth/signup";
 	}
-	
+
 	@PostMapping("/auth/signup")
 	public String signup(@Valid SignupDto signupDto, BindingResult bindingResult) {
-		
-		if(bindingResult.hasErrors()) {
-			Map<String, String> errorMap = new HashMap<>();
-			
-			for(FieldError error : bindingResult.getFieldErrors()) {
-				errorMap.put(error.getField(), error.getDefaultMessage());
-			}
-			throw new CustomValidationException("유효성 검사 실패함", errorMap);
-		} else {
-			User user = signupDto.toEntity();
-			User userEntity = authService.회원가입(user);
-			return "auth/signin";
-		}
+
+		User user = signupDto.toEntity();
+		authService.회원가입(user);
+		return "auth/signin";
 	}
 }
